57afc28a79aa64f803a58ef7c6510c03534483bb
zach balder <zmbalder@gmail.com>
2020-06-01 13:24:31 -0400

    initial

---
+++ README.md
@@ -0,0 +1,20 @@
+# giturls
+
+A command-line tool that searches GitHub for URLs belonging to a domain
+
+## Install
+
+`$ npm i giturls`
+
+## Usage
+
+```
+Usage: giturls [options] <domain>
+
+Options:
+  -V, --version          output the version number
+  -c, --cookie <string>  cookie for your GitHub account
+  -q, --quiet            don't show banner and info
+  -w, --window           open the browser window
+  -h, --help             display help for command
+```
---
+++ index.js
@@ -0,0 +1,155 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const commander = require('commander')
+const fs = require('fs')
+const path = require('path')
+const ProgressBar = require('progress')
+const puppeteer = require('puppeteer')
+
+const banner = fs.readFileSync(path.join(__dirname, 'banner'), 'utf8')
+const error = msg => console.error('\x1b[31m%s\x1b[0m', msg)
+const warn = msg => console.warn('\x1b[33m%s\x1b[0m', msg)
+
+const program = new commander.Command()
+
+const goto = async (page, url) => {
+  await page.goto(url)
+  await new Promise(resolve => setTimeout(resolve, 1e3))
+}
+
+const matchAll = (regex, string) => {
+  const matches = []
+
+  let match
+
+  while ((match = regex.exec(string))) {
+    matches.push(match)
+  }
+
+  return matches
+}
+
+const getNumPages = async (domain, page) => {
+  await goto(page, `https://github.com/search?q=${domain}&type=Code`)
+
+  const promise = page.$$eval('div.pagination > a', anchors => {
+    return anchors.map(a => (a.getAttribute('aria-label') || '').split(' ').pop())
+  })
+
+  return Math.max(...await promise)
+}
+
+const gotoPage = async (domain, page, pageNum) => {
+  await goto(page, `https://github.com/search?p=${pageNum}&q=${domain}&type=Code`)
+
+  let hrefs = await page.$$eval('div.f4 > a', anchors => {
+    return anchors.map(a => {
+      let url
+
+      try {
+        url = new URL(a.href)
+      } catch {
+        const href = a.href[0] === '/' ? a.href : '/' + a.href
+        url = new URL('https://github.com' + href)
+      }
+
+      url.hostname = 'raw.githubusercontent.com'
+      url.pathname = url.pathname.replace('/blob', '')
+
+      return url.href
+    })
+  })
+
+  hrefs = hrefs.filter(Boolean)
+
+  if (!hrefs.length) {
+    const content = await page.content()
+
+    if (content.includes('abuse detection')) {
+      error('[!] GitHub is rate-limiting you')
+    } else {
+      error(content)
+    }
+
+    return
+  }
+
+  for (const href of hrefs) {
+    try {
+      await goto(page, href)
+    } catch (err) {
+      error('[!] ' + err.message)
+      continue
+    }
+
+    const regex = /https?:\/\/[^\s,'"]+/g
+    const string = await page.$eval('pre', pre => pre.textContent)
+    const matches = matchAll(regex, string)
+
+    matches.forEach(([url]) => {
+      try {
+        url = new URL(url)
+        url.hostname.endsWith(domain) && console.log(url.href)
+      } catch {}
+    })
+  }
+}
+
+program
+  .version('0.0.0')
+  .arguments('<domain>')
+  .option('-c, --cookie <string>', 'cookie for your GitHub account')
+  .option('-q, --quiet', 'don\'t show banner and info')
+  .option('-w, --window', 'open the browser window')
+  .action(async (domain, opts) => {
+    const cookies = (opts.cookie || '')
+      .split(';')
+      .map(cookie => {
+        let [name, ...value] = cookie.split('=')
+        name = name && name.trim()
+        value = value.join('=').trim()
+        return name && value && { name, value }
+      })
+      .filter(Boolean)
+
+    error(banner)
+
+    const browser = await puppeteer.launch({ headless: !opts.window })
+    const page = await browser.newPage()
+
+    if (!opts.quiet) {
+      warn('[-] Searching GitHub...')
+      warn('[-] Target domain: ' + domain)
+    }
+
+    await goto(page, 'https://github.com')
+    await page.setCookie(...cookies)
+    const numPages = await getNumPages(domain, page)
+
+    let bar
+
+    if (!opts.quiet) {
+      warn(`[-] Found ${numPages} pages of search results`, opts)
+
+      bar = new ProgressBar('[:bar] :percent', {
+        complete: '=',
+        incomplete: ' ',
+        total: numPages,
+        width: 30
+      })
+    }
+
+    for (let pageNum = 1; pageNum <= numPages; pageNum++) {
+      await gotoPage(domain, page, pageNum)
+      opts.quiet || bar.tick()
+    }
+
+    opts.quiet || warn('[-] Done!')
+
+    await browser.close()
+  })
+  .parseAsync(process.argv)
+  .catch(err => error(err) || 1)
+  .then(process.exit)
